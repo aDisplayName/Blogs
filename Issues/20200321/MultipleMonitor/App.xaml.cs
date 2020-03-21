14ef41e6400ccd19c39443243a762ad8f7cc7db3
Matt Ali <mattali@gmail.com>
2020-03-21 12:18:25 +0800

    Add a inspection app.

---
+++ Issues/20200321/MultipleMonitor/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultipleMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var allScreens = System.Windows.Forms.Screen.AllScreens;
+            var info=allScreens.Aggregate(new StringBuilder(), (sb, screen) =>
+                    sb.AppendLine(
+                        $@"{screen.DeviceName}, IsPrimary: {(screen.Primary ? "True" : "False")}, Bounds: {screen.Bounds}, Working Area {screen.WorkingArea}")
+                ,sb=> sb.ToString()
+            );
+            Console.WriteLine(@"{0}", info);
+            var win = new MainWindow
+            {
+                Height = 500,
+                Width = 500,
+                Left = 0,
+                Top = 0,
+                SysInfo = {Text = info}
+            };
+            win.ShowDialog();
+
+            Shutdown();
+        }
+    }
+}
--- Issues/20200321/NavigatingDpiAwareScreenCoordinates.md
+++ Issues/20200321/NavigatingDpiAwareScreenCoordinates.md
@@ -1,1 +1,4 @@
 # Navigating DPI Aware Screen Coordinates
+
+
+Recently I needed to figure out how to create a new WPF window and precisely position the new window in anywhere 
