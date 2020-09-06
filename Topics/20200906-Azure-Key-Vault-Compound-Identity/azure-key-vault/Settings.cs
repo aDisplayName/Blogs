49801809c9779b2aa60f0a7a768c6d43cfce96f3
Lietian Ma <LMa2@slb.com>
2020-09-06 17:43:28 +0800

    Add more code

---
+++ Topics/20200906-Azure-Key-Vault-Compound-Identity/azure-key-vault/Settings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace azure_key_vault
+{
+    public class Settings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+        public string KeyVaultBaseUri { get; set; }
+
+        public string AppClientScope { get; set; }
+        public string Instance { get; set; }
+        public string SecretName { get; set; }
+
+        public static Settings LoadFromConfiguration(IConfiguration config)
+        {
+            return config.Get<Settings>();
+        }
+    }
+}
