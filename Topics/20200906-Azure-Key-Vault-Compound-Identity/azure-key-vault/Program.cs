65cefd3ef65ef1295cc24794b6a279cab10ecfe6
aDisplayName <mattali@gmail.com>
2020-09-06 16:53:47 +0800

    keyvault initial update

---
+++ Topics/20200906-Azure-Key-Vault-Compound-Identity/azure-key-vault/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace azure_key_vault
+{
+    public class Settings
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string Tenantid { get; set; }
+        public string KeyVaultBaseUri { get; set; }
+
+        public string AppClientScope { get; set; }
+        public string Instance { get; set; }
+        public string SecretName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
